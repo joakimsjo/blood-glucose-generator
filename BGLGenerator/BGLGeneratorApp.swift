6ff1773d42c44213faff8fbb06572f9ce588fa56
Joakim Sjøhaug <joakim@sjoha.ug>
2021-02-22 13:00:08 +0100

    :tada: init project

---
+++ BGLGenerator/BGLGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BGLGeneratorApp.swift
+//  BGLGenerator
+//
+//  Created by Joakim Sjøhaug on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct BGLGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
